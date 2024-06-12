f174557cef38681b69bab9eabd0afc0137df8ae0
exkludera <51145038+exkludera@users.noreply.github.com>
2024-06-12 10:21:07 +0200

    Update CTBans.cs

--- CTBans.cs
+++ CTBans.cs
@@ -2,28 +2,15 @@ using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes.Registration;
 using CounterStrikeSharp.API.Modules.Commands;
-using CounterStrikeSharp.API.Modules.Admin;
-using CounterStrikeSharp.API.Modules.Utils;
-using CounterStrikeSharp.API.Modules.Menu;
-using CounterStrikeSharp.API.Core.Attributes;
-using CounterStrikeSharp.API.Modules.Timers;
-using System.ComponentModel;
-using CounterStrikeSharp.API.Modules.Memory;
 using Nexd.MySQL;
-using CounterStrikeSharp.API.Modules.Entities;
-using System;
-using System.Drawing;
 
 namespace CTBans;
-[MinimumApiVersion(100)]
 
 public static class GetUnixTime
 {
     public static int GetUnixEpoch(this DateTime dateTime)
     {
-        var unixTime = dateTime.ToUniversalTime() -
-                       new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-
+        var unixTime = dateTime.ToUniversalTime() - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
         return (int)unixTime.TotalSeconds;
     }
 }
@@ -31,9 +18,9 @@ public static class GetUnixTime
 public partial class CTBans : BasePlugin, IPluginConfig<ConfigBan>
 {
     public override string ModuleName => "CTBans";
-    public override string ModuleAuthor => "DeadSwim";
+    public override string ModuleAuthor => "DeadSwim, modified by exkludera";
     public override string ModuleDescription => "Banning players to join in CT.";
-    public override string ModuleVersion => "V. 1.0.0";
+    public override string ModuleVersion => "V. 1.0.1";
 
     private static readonly bool?[] banned = new bool?[64];
     private static readonly string?[] remaining = new string?[64];
@@ -41,9 +28,7 @@ public partial class CTBans : BasePlugin, IPluginConfig<ConfigBan>
     private static readonly int?[] Showinfo = new int?[64];
     private static readonly bool?[] session = new bool?[64];
 
-
-    public ConfigBan Config { get; set; }
-
+    public required ConfigBan Config { get; set; }
 
     public void OnConfigParsed(ConfigBan config)
     {
@@ -58,38 +43,46 @@ public partial class CTBans : BasePlugin, IPluginConfig<ConfigBan>
         AddCommandListener("jointeam", OnPlayerChangeTeam);
         RegisterListener<Listeners.OnTick>(() =>
         {
-        for (int i = 1; i < Server.MaxPlayers; i++)
-        {
-            var ent = NativeAPI.GetEntityFromIndex(i);
-            if (ent == 0)
-                continue;
-            var client = new CCSPlayerController(ent);
-            if (client == null || !client.IsValid)
-                continue;
+            for (int i = 1; i < Server.MaxPlayers; i++)
+            {
+                var ent = NativeAPI.GetEntityFromIndex(i);
+                if (ent == 0)
+                    continue;
+
+                var client = new CCSPlayerController(ent);
+                if (client == null || !client.IsValid)
+                    continue;
+
                 if (Showinfo[client.Index] == 1)
                 {
-                    client.PrintToCenterHtml(
-                            $"<img src='https://icons.iconarchive.com/icons/paomedia/small-n-flat/48/sign-ban-icon.png'><br><br>" +
-                            $"You are <font color='red'>banned</font> to join in <font color='blue'>CT</font>!<br>" +
-                            $"<font color='green'>Remaining time</font> <font color='red'>{remaining[client.Index]}</font><br>" +
-                            $"<font color='green'>Reason of ban</font>  <font color='red'>{reason[client.Index]}</font><br>");
+                    client.PrintToCenterHtml
+                    (
+                        $"<img src='https://icons.iconarchive.com/icons/paomedia/small-n-flat/48/sign-ban-icon.png'><br><br>" +
+                        $"You are <font color='red'>banned</font> from <font color='blue'>CT</font>!<br>" +
+                        $"<font>time left: </font> <font color='red'>{remaining[client.Index]}</font><br>" +
+                        $"<font>reason: </font>  <font color='red'>{reason[client.Index]}</font><br>"
+                    );
+
                     AddTimer(10.0f, () =>
                     {
                         Showinfo[client.Index] = null;
                     });
                 }
-        }
+            }
         });
 
     }
     [GameEventHandler]
     public HookResult OnPlayerConnect(EventPlayerConnectFull @event, GameEventInfo info)
     {
-        CCSPlayerController player = @event.Userid;
+        CCSPlayerController player = @event.Userid!;
+
         if (player == null || !player.IsValid)
             return HookResult.Continue;
+
         var client = player.Index;
-        if(CheckBan(player) == true)
+
+        if (CheckBan(player) == true)
         {
             var timeRemaining = DateTimeOffset.FromUnixTimeSeconds(GetPlayerBanTime(player)) - DateTimeOffset.UtcNow;
             var nowtimeis = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
@@ -100,7 +93,7 @@ public partial class CTBans : BasePlugin, IPluginConfig<ConfigBan>
             {
                 MySqlDb MySql = new MySqlDb(Config.DBHost, Config.DBUser, Config.DBPassword, Config.DBDatabase);
                 var steamid = player.SteamID.ToString();
-                MySql.Table("deadswim_ctbans").Where($"ban_steamid = '{steamid}'").Delete();
+                //MySql.Table($"{Config.DBTable}").Where($"steamid = '{steamid}'").Delete();
                 banned[client] = false;
                 remaining[client] = null;
                 reason[client] = null;
@@ -123,23 +116,22 @@ public partial class CTBans : BasePlugin, IPluginConfig<ConfigBan>
         }
         return HookResult.Continue;
     }
+
     [GameEventHandler]
     public HookResult OnPlayerChangeTeam(CCSPlayerController? player, CommandInfo command)
     {
         var client = player!.Index;
 
         if (!Int32.TryParse(command.ArgByIndex(1), out int team_switch))
-        {
             return HookResult.Continue;
-        }
 
         if (player == null || !player.IsValid)
             return HookResult.Continue;
+
         CheckIfIsBanned(player);
 
         CCSPlayerPawn? playerpawn = player.PlayerPawn.Value;
         var player_team = team_switch;
-
 
         if(player_team == 3)
         {
@@ -153,4 +145,5 @@ public partial class CTBans : BasePlugin, IPluginConfig<ConfigBan>
 
         return HookResult.Continue;
     }
+
 }
